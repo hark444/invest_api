2371bcc380212eed411188ce01dfabbafaa6de3c
Harshad Keshwani <hkeshwani68@gmail.com>
2023-09-20 16:22:10 +0530

    Created fixed deposits table

---
+++ models/fixed_deposits.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, BigInteger, ForeignKey
+from models.user import UserModel
+from models.base_models import DepositsBaseModel, TimeStampBaseModel
+
+
+class FixedDeposits(DepositsBaseModel, TimeStampBaseModel):
+    __tablename__ = "fixed_deposits"
+
+    user_id = Column(BigInteger, ForeignKey("account_user.id"))
+    user = relationship(UserModel)
+    initial_investment = Column(Integer, nullable=False, doc="Initial amount that was invested as FD")

29e9638e5971b5f915cc2caf40b680152ab6da69
Harshad Keshwani <hkeshwani68@gmail.com>
2023-09-25 01:28:21 +0530

    Added PUT endpoint for dividends

--- app/api/v1/routes/dividends.py
+++ app/api/v1/routes/dividends.py
@@ -65,33 +65,31 @@ async def get_all_fd_by_user(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
         )
 
-"""
-@fixed_deposit_router.put('/{deposit_id}', response_model=FixedDepositsResponseSchema)
+
+@dividends_router.put('/{dividend_id}', response_model=DividendsResponseSchema)
 async def update_fixed_deposit(
-        deposit_id: int, fd: FixedDepositsRequestSchema, db: Session = Depends(get_db),
+        dividend_id: int, dividend: DividendsRequestSchema, db: Session = Depends(get_db),
         user: UserModel = Depends(get_current_user)
 ):
 
-    existing_deposit_obj = db.query(FixedDeposits).filter_by(id=deposit_id, user_id=user.id).first()
-    if not existing_deposit_obj:
+    existing_dividends_obj = db.query(Dividends).filter_by(id=dividend_id, user_id=user.id).first()
+    if not existing_dividends_obj:
         raise HTTPException(status_code=404, detail="Fixed Deposit object not found for this user and deposit id")
     try:
-        total_profit = get_total_profit(fd)
 
-        for field, value in dict(fd).items():
-            setattr(existing_deposit_obj, field, value)
-        existing_deposit_obj.total_profit = total_profit
+        for field, value in dict(dividend).items():
+            setattr(existing_dividends_obj, field, value)
 
-        db.add(existing_deposit_obj)
+        db.add(existing_dividends_obj)
         db.commit()
-        db.refresh(existing_deposit_obj)
-        return existing_deposit_obj
+        db.refresh(existing_dividends_obj)
+        return existing_dividends_obj
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
         )
 
-
+"""
 @fixed_deposit_router.delete('/{deposit_id}', status_code=200)
 def delete_deposit(
         deposit_id: int,

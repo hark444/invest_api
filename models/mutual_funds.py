a258977787badeb9ce5d2835ee10110550bfb435
Harshad Keshwani <hkeshwani68@gmail.com>
2024-02-26 16:35:43 +0530

    Added MF Models

--- models/mutual_funds.py
+++ models/mutual_funds.py
@@ -4,7 +4,8 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, BigInteger, ForeignKey, Float, String, DateTime, Enum
 from models.user import UserModel
 from models.base_models import TimeStampBaseModel
-from enum import Enum
+# from enum import Enum
+from datetime import datetime
 
 
 class MFInvestmentTypes(str, enum.Enum):
@@ -13,9 +14,19 @@ class MFInvestmentTypes(str, enum.Enum):
     REGULAR = "REGULAR"
 
 
+class MFInvestments(TimeStampBaseModel):
+    __tablename__ = "mf_investments"
+
+    id = Column(Integer, primary_key=True, index=True)
+    investment_amount = Column(Float, nullable=False)
+    investment_date = Column(DateTime, default=datetime.now())
+    remarks = Column(String, nullable=True)
+
+
 class MutualFunds(TimeStampBaseModel):
     __tablename__ = "mutual_funds"
 
+    id = Column(Integer, primary_key=True, index=True)
     user_id = Column(BigInteger, ForeignKey("account_user.id"))
     user = relationship(UserModel)
     name = Column(String, nullable=False)
@@ -23,8 +34,8 @@ class MutualFunds(TimeStampBaseModel):
     investment_type = Column(Enum(MFInvestmentTypes), nullable=False, server_default="REGULAR")
     broker_name = Column(String, nullable=True)
     total_investment = Column(Float, default=0.0)
-    investments = relationship("MFInvestments", back_populates="mf_investments")
-
-#
-# class MFInvestments(TimeStampBaseModel):
-
+    mf_investments_id = Column(BigInteger, ForeignKey("mf_investments.id"))
+    mf_investments = relationship(MFInvestments)
+    current_nav = Column(Float, nullable=True)
+    return_on_investment = Column(Float, nullable=True)
+    cagr = Column(Float, nullable=True)
